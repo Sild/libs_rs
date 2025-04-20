3c32018f79f775010ee8fbadce964ac61484f1ff
Sild <sildtm@icloud.com>
2025-04-20 15:12:20 +0200

    add dict benches

--- ton_lib/benches/build_dict_cell.rs
+++ ton_lib/benches/build_dict_cell.rs
@@ -1,75 +1,47 @@
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use std::collections::HashMap;
+use std::sync::LazyLock;
 use ton_lib::cell::build_parse::builder::CellBuilder;
+use ton_lib::tlb::adapters::dict_key_adapters::DictKeyAdapterInto;
+use ton_lib::tlb::adapters::dict_val_adapters::DictValAdapterNum;
+use ton_lib::tlb::adapters::Dict;
+use tonlib_core::cell::dict::predefined_writers::val_writer_unsigned_min_size;
 use tonlib_core::cell::CellBuilder as TonlibCellBuilder;
 
-const ITERATIONS_COUNT: usize = 100;
+const ITERATIONS_COUNT: usize = 1;
+const DICT_ITEMS_COUNT: usize = 100;
 
-fn build_empty_cell_ton_lib() {
+static DICT_DATA: LazyLock<HashMap<usize, usize>> = LazyLock::new(|| {
+    let mut dict = HashMap::new();
+    for i in 0..DICT_ITEMS_COUNT {
+        dict.insert(i, 3);
+    }
+    dict
+});
+
+fn build_dict_ton_lib() {
     for _ in 0..ITERATIONS_COUNT {
         let mut builder = CellBuilder::new();
-        builder.write_ref(CellBuilder::new().build().unwrap().into_ref()).unwrap();
+        let data_clone = DICT_DATA.clone(); // must do it to compare with tonlib_core
+        Dict::<DictKeyAdapterInto, DictValAdapterNum<2>, _, _>::new(256).write(&mut builder, &data_clone).unwrap();
         let cell = builder.build().unwrap();
         black_box(cell);
     }
 }
 
-fn build_empty_cell_tonlib_core() {
+fn build_dict_tonlib_core() {
     for _ in 0..ITERATIONS_COUNT {
         let mut builder = TonlibCellBuilder::new();
-        builder.store_child(TonlibCellBuilder::new().build().unwrap()).unwrap();
-        let cell = builder.build().unwrap();
-        black_box(cell);
-    }
-}
-
-fn build_full_cell_ton_lib() {
-    for _ in 0..ITERATIONS_COUNT {
-        let mut builder1 = CellBuilder::new();
-        builder1.write_bits([1, 2, 3], 24).unwrap();
-
-        let mut builder2 = CellBuilder::new();
-        builder2.write_bits([10, 20, 30], 24).unwrap();
-
-        let mut builder3 = CellBuilder::new();
-        builder3.write_bits([100, 200, 255], 24).unwrap();
-
-        let mut builder = CellBuilder::new();
-        builder.write_ref(builder1.build().unwrap().into_ref()).unwrap();
-        builder.write_ref(builder2.build().unwrap().into_ref()).unwrap();
-        builder.write_ref(builder3.build().unwrap().into_ref()).unwrap();
-
-        let cell = builder.build().unwrap();
-        black_box(cell);
-    }
-}
-
-fn build_full_cell_tonlib_core() {
-    for _ in 0..ITERATIONS_COUNT {
-        let mut builder1 = TonlibCellBuilder::new();
-        builder1.store_slice(&[1, 2, 3]).unwrap();
-
-        let mut builder2 = TonlibCellBuilder::new();
-        builder2.store_slice(&[10, 20, 30]).unwrap();
-
-        let mut builder3 = TonlibCellBuilder::new();
-        builder3.store_slice(&[100, 200, 255]).unwrap();
-
-        let mut builder = TonlibCellBuilder::new();
-        builder.store_child(builder1.build().unwrap()).unwrap();
-        builder.store_child(builder2.build().unwrap()).unwrap();
-        builder.store_child(builder3.build().unwrap()).unwrap();
-
+        let data_clone = DICT_DATA.clone();
+        builder.store_dict(256, val_writer_unsigned_min_size, data_clone).unwrap();
         let cell = builder.build().unwrap();
         black_box(cell);
     }
 }
 
 fn benchmark_functions(c: &mut Criterion) {
-    c.bench_function("build_empty_cell_ton_lib", |b| b.iter(build_empty_cell_ton_lib));
-    c.bench_function("build_empty_cell_tonlib_core", |b| b.iter(build_empty_cell_tonlib_core));
-
-    c.bench_function("build_full_cell_ton_lib", |b| b.iter(build_full_cell_ton_lib));
-    c.bench_function("build_full_cell_tonlib_core", |b| b.iter(build_full_cell_tonlib_core));
+    c.bench_function("build_dict_ton_lib", |b| b.iter(build_dict_ton_lib));
+    c.bench_function("build_dict_tonlib_core", |b| b.iter(build_dict_tonlib_core));
 }
 
 criterion_group!(benches, benchmark_functions);
